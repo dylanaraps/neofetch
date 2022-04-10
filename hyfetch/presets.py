a2c8e99f39bcb79b3c0aced4b18ed950c0e82291
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-04-10 03:19:01 -0400

    [+] Decode ColorProfile to RGB

--- hyfetch/presets.py
+++ hyfetch/presets.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
 from typing import Literal
 
+from .color_util import RGB
+
 
 @dataclass
 class ColorProfile:
     colors: list[str]
     spacing: Literal['equal', 'weighted'] = 'equal'
+
+    def decode(self) -> list[RGB]:
+        """
+        Decode to a list of RGBs
+
+        :return: List of RGBs
+        """
+        return [RGB.from_hex(c) for c in self.colors]
