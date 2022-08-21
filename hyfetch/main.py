6911087eaba323dd54d369f2d50374fff9d60838
Hykilpikonna <me@hydev.org>
2022-08-20 20:46:56 -0400

    [+] Add --ask-exit option

--- hyfetch/main.py
+++ hyfetch/main.py
@@ -336,6 +336,7 @@ def run():
     parser.add_argument('--debug', action='store_true', help=f'Debug mode')
     parser.add_argument('--test-distro', help=f'Test for a specific distro')
     parser.add_argument('--test-print', action='store_true', help=f'Test print distro ascii art only')
+    parser.add_argument('--ask-exit', action='store_true', help=f'Ask before exitting')
 
     args = parser.parse_args()
 
@@ -385,3 +386,6 @@ def run():
 
     # Run
     run_neofetch(preset, config.color_align)
+
+    if args.ask_exit:
+        input('Press any key to exit...')
