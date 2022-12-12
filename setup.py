51b8bd61e0cbf2b81220c2484d98eae610aa2a12
Hykilpikonna <me@hydev.org>
2022-12-11 21:35:20 -0500

    [F] Fix subpackage installing
    https://github.com/dylanaraps/neofetch/issues/2257

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pathlib
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import hyfetch.constants
 
@@ -30,7 +30,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    packages=['hyfetch'],
+    packages=find_packages(),
     package_data={'hyfetch': ['hyfetch/*']},
     include_package_data=True,
     install_requires=[
