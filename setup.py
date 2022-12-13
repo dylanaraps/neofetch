244efd541eff6857f0e55218533f2ec199d61a88
Hykilpikonna <me@hydev.org>
2022-12-12 19:21:09 -0500

    [+] Add python 3.11 tag

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=find_packages(),
     package_data={'hyfetch': ['hyfetch/*']},
